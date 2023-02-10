f27de5b2d0913f469cd7fa5ece9a44bc73f9fa2a
AstonDV <astondv.kms@gmail.com>
2023-02-11 00:46:55 +1000

    Stop task 5

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вывести первые N строк треугольника Паскаля. Сделать 
+// вывод в виде равнобедренного треугольника
+
+
+int Input(string text)
+{
+    Console.Write($"{text}: ");
+    int value = int.Parse(Console.ReadLine()!);
+    return value;
+}
+
+void Main()
+{
+    Console.Clear();
+    int height = Input("Введите высоту треугольника Паскаля");
+}
+
+Main();
